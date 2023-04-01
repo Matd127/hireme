c0de7db495422a13d198fc398d842d4fbd380d70
Matd127 <79983536+Matd127@users.noreply.github.com>
2023-04-01 16:36:28 +0200

    Create contact form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,11 +1,33 @@
-import { FormContainer, Form } from "./ContactForm.style";
+import { FormContainer, Form, SubmitButton, FormTitle, FormField, FormItem, FormLabel, FormMessage } from "./ContactForm.style";
 
 const ContactForm = () => {
   return (
     <>
       <FormContainer>
         <Form>
-            <h1>Contact with us</h1>
+            <FormTitle>Contact US</FormTitle>
+
+            <FormItem>
+              <FormLabel>Your name</FormLabel>
+              <FormField type="text"></FormField>
+            </FormItem>
+
+            <FormItem>
+              <FormLabel>Your email</FormLabel>
+              <FormField type="text"></FormField>
+            </FormItem>
+
+            <FormItem>
+              <FormLabel>Your phone</FormLabel>
+              <FormField type="text"></FormField>
+            </FormItem>
+
+            <FormItem>
+              <FormLabel>Message</FormLabel>
+              <FormMessage rows={6}/>
+            </FormItem>
+
+          <SubmitButton onClick={(e) => e.preventDefault()}>Submit</SubmitButton>
         </Form>
       </FormContainer>
     </>
--- src/components/ContactForm/ContactForm.style.js
+++ src/components/ContactForm/ContactForm.style.js
@@ -1,17 +1,65 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const FormContainer = styled.div`
   width: 100%;
   display: grid;
   justify-items: center;
   color: white;
+  text-align: center;
 `;
 
 export const Form = styled.form`
   margin: 1rem;
   padding: 1rem;
-  height: 400px;
+  min-height: 300px;
+  min-width: 30vw;
   background-color: #1a1e27;
   border: 1px solid rgba(129, 129, 129, 0.25);
-  border-radius: 1px;
+  border-radius: 8px;
+`;
+
+export const FormTitle = styled.h1`
+  font-size: 35px;
+`;
+export const FormItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  padding: 10px;
+`;
+
+export const FormLabel = styled.label``;
+
+const baseInputStyles = css`
+  margin-top: 5px;
+  background-color: transparent;
+  border: 1px solid rgba(129, 129, 129, 0.30);
+  border-radius: 3px;
+  color: white;
+  font-size: 16px;
+
+  &:focus{
+    outline: none;
+    border: 1px solid rgba(129, 129, 129, 0.80);
+  }
+`
+
+export const FormField = styled.input`
+  ${baseInputStyles}
+`;
+
+export const FormMessage = styled.textarea`
+  ${baseInputStyles}
+  resize: none;
+`
+
+export const SubmitButton = styled.button`
+  border: none;
+  font-weight: bold;
+  font-size: 20px;
+  background-color: rgb(236, 53, 67);
+  color: white;
+  border-radius: 4px;
+  padding: 10px;
+  cursor: pointer;
 `;
